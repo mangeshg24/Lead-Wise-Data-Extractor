760e39c9f8653367e1be296f8e36c94564c48a4e
Mangesh Gaykar <mangeshgaykar24@gmail.com>
2025-12-07 10:26:05 +0530

    Clean Vite config for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/label-processing-dashboard/",
   plugins: [react()],
+  base: "/label-processing-dashboard/",
 });
